2f93eca2aa85219249033480265cc887add2dfb5
Paweł Maczuga <pmaczuga97@gmail.com>
2023-04-21 15:34:07 +0200

    Feat: Better plots
    - More descriptive axes and titles
    - Saving plots
    - Plot of derivative of exact solution
    - Error and loss without filter

--- plotting.py
+++ plotting.py
@@ -1,5 +1,8 @@
 import argparse
+import math
+import os
 import matplotlib
+from matplotlib.figure import Figure
 import matplotlib.pyplot as plt
 from matplotlib.pyplot import draw, figure, show
 import numpy as np
@@ -9,6 +12,7 @@ from src.analytical import AnalyticalDelta, analytical_from_params
 from src.params import Params
 from src.io_utils import load_result
 from src.pinn import dfdx, f
+from src.utils import get_tag_path
 
 matplotlib.rcParams['text.latex.preamble'] = [r'\usepackage{amsmath}']
 
@@ -18,6 +22,14 @@ parser = argparse.ArgumentParser(
                     description='Runs the training of DRVPINN')
 parser.add_argument('--tag', type=str)
 args = parser.parse_args()
+
+def save_fig(fig: Figure, tag: str, filename: str) -> str:
+    directory = f"{get_tag_path(tag)}/figures"
+    if not os.path.exists(directory):
+        os.mkdir(directory)
+    filename = f"{directory}/{filename}"
+    fig.savefig(filename, bbox_inches='tight', dpi=200)
+    return filename
 
 # Here you can set tag - that is directory inside results where the 
 # tag = "tmp"
@@ -30,7 +42,7 @@ loss_vector = result.loss
 error_vector = result.error
 norm_vector = result.norm
 
-x_domain = [-1., 1.0]; n_points_x=params.n_points_x 
+x_domain = [-1., 1.0]; n_points_x=1000
 x_raw = torch.linspace(x_domain[0], x_domain[1], steps=n_points_x)
 x_raw.requires_grad_()
 x = x_raw.reshape(-1, 1)
@@ -50,72 +62,110 @@ matplotlib.rc('text', usetex=True)
 font = {'family' : 'sans-serif', 'size' : 15}
 matplotlib.rc('font', **font)
 
-fig, ax = plt.subplots() #figure(figsize=(7, 7))
+##########################################################################
+# Result and exact solution
+##########################################################################
+fig, ax = plt.subplots()
 ax.plot(x_draw.detach().cpu(), y_draw.detach().cpu(),'b-', label = 'DRVPINNs',linewidth = 2)
-# ax.plot(xnp1, y_ana_np1,'r--',linewidth = 2)
-# ax.plot(xnp2, y_ana_np2,'r--', label = 'Analytical',linewidth = 2)
 ax.plot(x_draw, y_ana_np, 'r--', label="Analytical", linewidth=2)
-
-ax.legend(loc='upper left')#, fontsize='x-large')
-#plt.plot(x_draw.detach().cpu(), y_ana.detach().cpu(),'r--')
+ax.legend(loc='upper left')
 ax.set_xlabel(r" $x$ ", size = 22)
 ax.set_ylabel(r" $u$ ", size = 22)
+ax.set_title("Result and exact solution")
+save_fig(fig, tag, "result.png")
 
 ##########################################################################
-
-figure(figsize=(10, 10))
-delta_approx = dfdx(pinn, x, order=1)
-plt.plot(x_draw.detach().cpu(), delta_approx.detach().cpu(),'b-')
-#plt.semilogy(x_draw.detach().cpu(), abs(y_ana.detach().cpu()-y_draw.detach().cpu()),'b-')
+# Derivative of result and exact solution
+##########################################################################
+fig, ax = plt.subplots()
+pinn_dx = dfdx(pinn, x, order=1)
+exact_dx = analytical.dx(x)
+ax.plot(x_draw.detach().cpu(), pinn_dx.detach().cpu(),'b-', label = 'DRVPINN dx')
+ax.plot(x_draw.detach().cpu(), exact_dx.detach().cpu(),'r--', label='Analytical dx')
+ax.set_title("Derivative of result and exact solution")
+ax.legend(loc='lower left')#, fontsize='x-large')
+save_fig(fig, tag, "result-derivative.png")
 
 ##########################################################################
-
 vec = loss_vector
-best = 1
+best = math.inf
 best_vec = [1.]
 pos_vec = [1.]
+epochs_vector = np.array(range(1, params.epochs + 1))
 
 
-
-for n in range(1,params.epochs):
+for n in range(params.epochs):
   if vec[n]<best and vec[n]>0:
     best_vec.append(vec[n])
     pos_vec.append(n+1)
     best = 1*vec[n]
 
 pos_vec = np.array(pos_vec, dtype=int) - 1
+##########################################################################
 
+##########################################################################
+# Loss and norm
+##########################################################################
 fig, ax = plt.subplots()
 ax.loglog(pos_vec, loss_vector[pos_vec],'b-',linewidth = 2, label="Loss")
 ax.loglog(pos_vec, norm_vector[pos_vec], 'r--', linewidth=2, label="Norm")
 ax.legend()
+ax.set_title("Loss and norm", size = 24)
 ax.set_xlabel(r" Iterations ", size = 22)
 ax.set_ylabel(r" Loss ", size = 22)
+save_fig(fig, tag, "loss-and-norm.png")
 
+##########################################################################
+# Loss without filter
+##########################################################################
+fig, ax = plt.subplots()
+ax.loglog(epochs_vector, loss_vector,'b-',linewidth = 1, label="Loss")
+ax.legend()
+ax.set_title("Loss no filer", size = 24)
+ax.set_xlabel(r" Iterations ", size = 22)
+ax.set_ylabel(r" Loss ", size = 22)
+save_fig(fig, tag, "loss-no-filter.png")
+
+##########################################################################
+# Error
+##########################################################################
 fig, ax = plt.subplots()
 ax.loglog(pos_vec, error_vector[pos_vec], 'g-', linewidth=2, label="Error")
 ax.set_xlabel(r" Iterations ", size = 22)
-ax.set_ylabel(r" Loss ", size = 22)
+ax.set_ylabel(r" Error ", size = 22)
 ax.set_title("Error")
+save_fig(fig, tag, "error.png")
 
-###########################################################
+##########################################################################
+# Error no fiter
+##########################################################################
+fig, ax = plt.subplots()
+ax.loglog(epochs_vector, error_vector, 'g-', linewidth=2, label="Error")
+ax.set_xlabel(r" Iterations ", size = 22)
+ax.set_ylabel(r" Error ", size = 22)
+ax.set_title("Error no filter")
+save_fig(fig, tag, "error-no-filter.png")
 
+##########################################################################
+# Error to sqrt(loss)
+##########################################################################
 fig, ax = plt.subplots()
 ax.loglog(np.sqrt(loss_vector[pos_vec]), error_vector[pos_vec])
 ax.loglog(np.sqrt(loss_vector[pos_vec]), np.sqrt(loss_vector[pos_vec]))
+ax.set_xlabel(r" $\sqrt{Loss}$ ", size = 22)
+ax.set_ylabel(r" Error ", size = 22)
+ax.set_title(r"Error to $\sqrt{Loss}$")
+filename = f"{get_tag_path(tag)}/error.pt"
+save_fig(fig, tag, "error-to-sqrt-loss.png")
 
-# if error_vector is not None:
-#   fig, ax = plt.subplots()
-#   ax.loglog(error_vector, 'b-', linewidth=2)
-#   ax.set_title("Error")
-#   ax.set_xlabel(r" Iterations ", size = 22)
-#   ax.set_ylabel(r" Error ", size = 22)
-
-# if norm_vector is not None:
-#   fig, ax = plt.subplots()
-#   ax.loglog(norm_vector, 'b-', linewidth=2)
-#   ax.set_title("Norm")
-#   ax.set_xlabel(r" Iterations ", size = 22)
-#   ax.set_ylabel(r" Norm ", size = 22)
+##########################################################################
+# Exact solution on right end
+##########################################################################
+# fig, ax = plt.subplots()
+# x_raw = torch.linspace(0.98, x_domain[1], steps=n_points_x)
+# x = x_raw.reshape(-1, 1)
+# x_draw = x.flatten().detach()
+# y_ana_np = analytical(x_draw)
+# ax.plot(x_draw, y_ana_np, 'r--', label="Analytical", linewidth=2)
 
 show()
